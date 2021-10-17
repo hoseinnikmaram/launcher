17608dd9a282785fbd93e29b111bbbc7784ddfaa
nikmaram <mhnikmaram1@gmail.com>
2021-10-17 15:25:48 +0330

    implement packages do not contain vpn

--- app/src/main/java/com/example/launcher/ui/MainFragment/MainViewModel.kt
+++ app/src/main/java/com/example/launcher/ui/MainFragment/MainViewModel.kt
@@ -2,31 +2,38 @@ package com.example.launcher.ui.MainFragment
 
 import android.app.Activity
 import android.content.Intent
+import android.content.pm.ResolveInfo
 import androidx.lifecycle.*
 import com.example.launcher.model.PackageModel
-import com.example.launcher.network.ApiRepository
 import com.example.launcher.util.*
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.withContext
-import java.lang.Exception
 
-class MainViewModel :ViewModel(){
+class MainViewModel : ViewModel() {
 
-     fun getInstalledPackage(activity: Activity) =
+    fun getInstalledPackage(activity: Activity): LiveData<List<PackageModel>> =
         liveData(Dispatchers.IO) {
-        val mainIntent = Intent(Intent.ACTION_MAIN, null)
-        mainIntent.addCategory(Intent.CATEGORY_LAUNCHER)
-        val pkgAppsList = activity.packageManager.queryIntentActivities(mainIntent, 0)
-        emit(pkgAppsList.map {
-            PackageModel(
-                icon = it.loadIcon(activity.packageManager),
-                label = it.loadLabel(activity.packageManager).toString(),
-                packageName = it.activityInfo.packageName
-            )
-        })
+            val packageModel = mutableListOf<PackageModel>()
+            val mainIntent = Intent(Intent.ACTION_MAIN, null)
+            mainIntent.addCategory(Intent.CATEGORY_LAUNCHER)
+            val pkgAppsList = activity.packageManager.queryIntentActivities(mainIntent, 0)
+            pkgAppsList.forEach {
+                if (!isVpnApplication(it, activity))
+                    packageModel.add(PackageModel(
+                        icon = it.loadIcon(activity.packageManager),
+                        label = it.loadLabel(activity.packageManager).toString(),
+                        packageName = it.activityInfo.packageName
+                    ))
+            }
+            emit(packageModel)
+        }
+
+    fun isVpnApplication(resolveInfo: ResolveInfo, activity: Activity): Boolean {
+        return resolveInfo.activityInfo.packageName.lowercase()
+            .contains("vpn") || resolveInfo.loadLabel(activity.packageManager).toString()
+            .contains("vpn")
     }
 
-    fun isShowDialog(activity: Activity){
+    fun isShowDialog(activity: Activity) {
         val isShowDialog = defaultCache(activity.applicationContext)[KEY_IS_SHOW_DIALOG, false]
         if (!isShowDialog!!) {
             defaultCache(activity.applicationContext)[KEY_IS_SHOW_DIALOG] = true
